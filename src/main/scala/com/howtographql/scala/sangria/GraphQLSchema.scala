823a9e988b5c6e27feed15af4a3a8a83474e927c
Mariusz Nosinski <mnosinski@gmail.com>
2018-06-25 16:52:57 +0200

    chapter 3

--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -2,23 +2,33 @@ package com.howtographql.scala.sangria
 
 import sangria.schema.{ListType, ObjectType}
 import models._
-// #
 import sangria.schema._
 import sangria.macros.derive._
 
 object GraphQLSchema {
 
-  // 1
   implicit val LinkType = deriveObjectType[Unit, Link]()
 
-  // 2
+
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
+
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.dao.getLink(c.arg(Id))
+      ),
+      Field("links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg(Ids))
+      )
     )
   )
 
-  // 3
   val SchemaDefinition = Schema(QueryType)
 }
